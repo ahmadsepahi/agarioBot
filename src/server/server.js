f79401575db35780918e1b1ca3fba279e6d501da
Nikita Kirilov <kirilov.dev@gmail.com>
2018-02-16 10:56:00 +0700

    init socket architecture

--- src/server/server.js
+++ src/server/server.js
@@ -1,29 +1,55 @@
 'use strict';
 
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var SocketsHandler = require('./sockets');
+var SAT = require('sat');
+var sql = require ("mysql");
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+// Import game settings.
+var c = require('../../config.json');
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+// Import utilities.
+var util = require('./lib/util');
+
+
+SocketsHandler.connect(http, function(io, socket) {
+  console.log('New user connected!', socket.handshake.query.type);
+
+  let type = socket.handshake.query.type;
+  let radius = util.massToRadius(c.defaultPlayerMass);
+  let position = c.newPlayerInitialPosition == 'farthest' ? util.uniformPosition(users, radius) : util.randomPosition(radius);
+
+  var cells = [];
+  var massTotal = 0;
+
+  if (type === 'player') {
+      cells = [{
+          mass: c.defaultPlayerMass,
+          x: position.x,
+          y: position.y,
+          radius: radius
+      }];
+      massTotal = c.defaultPlayerMass;
+  }
+
+  let currentPlayer = {
+      id: socket.id,
+      x: position.x,
+      y: position.y,
+      w: c.defaultPlayerMass,
+      h: c.defaultPlayerMass,
+      cells: cells,
+      massTotal: massTotal,
+      hue: Math.round(Math.random() * 360),
+      type: type,
+      lastHeartbeat: new Date().getTime(),
+      target: {
+          x: 0,
+          y: 0
+      }
+  };
+
+  SocketsHandler.handleAction(io, socket, currentPlayer);
 });
-
-let uuid = 0;
-
-io.on('connection', function(socket) {
-
-  socket.emit('assign uuid', { uuid: uuid });
-  uuid += 1;
-
-  socket.on('move', function(msg) {
-    console.log(msg.x, msg.y);
-
-    io.sockets.emit('update map', { uuid: uuid, x: msg.x, y: msg.y });
-  });
-});
-
-server.listen(8080, () => {
-  console.log('Server has started on port: 8080');
-})
