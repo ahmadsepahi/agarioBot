b8d3247b2b39b06b5576517e5fde0043d52d748a
Nikita Kirilov <kirilov.dev@gmail.com>
2018-02-16 10:55:46 +0700

    init serve architecture

---
+++ src/server/socket.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const socketio = require('socket.io')
+
+
+const util = require('./lib/util');
+
+
+exports.connect = function(http, callback) {
+    const io = socketio(http);
+
+    io.on('connection', function(socket) {
+        callback(io, socket);
+    });
+}
+
+
+exports.handleAction = function(io, socket, currentPlayer) {
+    socket.on('pingcheck', function () {
+        socket.emit('pongcheck');
+    });
+
+    socket.on('gotit', function (player) {
+        console.log('[INFO] Player ' + player.name + ' connecting!');
+    });
+
+    socket.on('windowResized', function (data) {
+        currentPlayer.screenWidth = data.screenWidth;
+        currentPlayer.screenHeight = data.screenHeight;
+    });
+
+    socket.on('respawn', function () {
+    });
+
+    socket.on('disconnect', function () {
+    });
+
+    socket.on('pass', function(data) {
+    });
+}
