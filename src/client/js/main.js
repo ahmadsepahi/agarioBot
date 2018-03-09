0dcb362510a3d2d9ddaa4d4785493f85bad286f5
Николай Нагуслаев <kolian02021@gmail.com>
2018-03-09 16:22:45 +0700

    canvas drawgrid part 1

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -68,9 +68,9 @@ window.onload = function() {
     var instructions = document.getElementById('rule');
 }
 
-window.Canvas = new Canvas();
+window.canvas = new Canvas();
 var c = window.canvas.cv;
-var graphic = c.getContext('2d');
+var graph = c.getContext('2d');
 
 
 window.requestAnimFrame = (function() {
@@ -90,7 +90,7 @@ function playLoop() {
     if (!global.disconnect) {
         if (global.gameStart) {
             graph.fillStyle = global.backgroundColor;
-            graph.fillRect(0, 0, global.screenWidth, global.screenHeight);
+            graph.fillRect(0, 0, global.scrWidth, global.scrHeight);
 
             //drawgrid();
            
@@ -98,6 +98,60 @@ function playLoop() {
         } 
         }
 }
+var myGridObject = {
+    canvasWidth : global.scrWidth, //ширина холста
+    canvasHeight : global.scrHeight, //высота холста
+    cellsNumberX : 15, //количество ячеек по горизонтали
+    cellsNumberY : 15, //количество ячеек по вертикали
+    color : "#000", //цвет линий
+        //Метод setSettings устанавливает все настройки
+    setSettings : function() {
+                // получаем наш холст по id
+        canvas = document.getElementById("mycanvas");
+                // устанавливаем ширину холста
+        canvas.width = this.canvasWidth;
+                // устанавливаем высоту холста
+        canvas.height = this.canvasHeight;
+                // canvas.getContext("2d") создает объект для рисования
+        ctx = canvas.getContext("2d");
+                // задаём цвет линий
+        ctx.strokeStyle = this.color;
+                // вычисляем ширину ячейки по горизонтали
+        lineX = canvas.width / this.cellsNumberX;
+                // вычисляем высоту ячейки по вертикали
+        lineY = canvas.height / this.cellsNumberY;
+    },
+        // данная функция как раз и будет отрисовывать сетку
+    drawGrid : function() {
+                // в переменной buf будет храниться начальная координата, откуда нужно рисовать линию
+                // с каждой итерацией она должна увеличиваться либо на ширину ячейки, либо на высоту
+        var buf = 0;
+        // Рисуем вертикальные линии
+        for (var i = 0; i <= this.cellsNumberX; i++) {
+                        // начинаем рисовать
+            ctx.beginPath();
+                        // ставим начальную точку
+            ctx.moveTo(buf, 0);
+                        // указываем конечную точку для линии
+            ctx.lineTo(buf, canvas.height);
+                        // рисуем и выводим линию
+            ctx.stroke();
+            buf +=lineX;
+        }       
+        buf = 0;
+        // Рисуем горизонтальные линии
+        for (var j = 0; j <= this.cellsNumberY; j++) {
+            ctx.beginPath();
+            ctx.moveTo(0, buf);
+            ctx.lineTo(canvas.width, buf);
+            ctx.stroke();
+            buf +=lineY;
+        }
+    }
+}
+myGridObject.setSettings();
+myGridObject.drawGrid();
+
 function setSocket(socket){
     socket.on('gameSetup', function(data) {
         global.gameWidth = data.gameWidth;
