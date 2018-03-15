d6e3737c1d0064dccbea3dbde345d8aca0dac79d
Nikita Kirilov <kirilov.dev@gmail.com>
2018-03-15 16:02:50 +0700

    pursue server sockets

--- src/server/server.js
+++ src/server/server.js
@@ -1,63 +1,98 @@
+/*jslint bitwise: true, node: true */
+'use strict';
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var SocketsHandler = require('./sockets');
-// var io = require('socket.io')(http);
+const io = require('socket.io')(http);
+
 
 // Import game settings.
-var config = require('../../config.json');
-
-
-var controllers = require('./controllers/controllers');
-var PlayerController = require('./controllers/player_controller');
+var c = require('../../config.json');
 
 // Import utilities.
 var util = require('./lib/util');
 
-var users = {};
+
+
+const UserController = require('./user_controller');
+
+var users = [];
+var massFood = [];
+var food = [];
 var sockets = {};
+
+var initMassLog = util.log(c.defaultPlayerMass, c.slowBase);
 
 app.use(express.static(__dirname + '/../client'));
 
-SocketsHandler.connect(http, function (io, socket) {
-  console.log('[INFO]: new user connected!', socket.id);
+io.on('connection', function(socket) {
+    console.log('A user connected!', socket.handshake.query.type);
 
-  let currentPlayer = new PlayerController.PlayerController(socket);
-  users[currentPlayer.id] = currentPlayer;
-  sockets[socket.id] = socket;
+    var type = socket.handshake.query.type;
+    var radius = util.massToRadius(c.defaultPlayerMass);
+    var position = c.newPlayerInitialPosition == 'farthest' ? util.uniformPosition(users, radius) : util.randomPosition(radius);
 
-  SocketsHandler.handleAction(io, socket, currentPlayer);
+    var cells = [];
+    var massTotal = 0;
+    if(type === 'player') {
+        cells = [{
+            mass: c.defaultPlayerMass,
+            x: position.x,
+            y: position.y,
+            radius: radius
+        }];
+        massTotal = c.defaultPlayerMass;
+    }
+
+    let currentPlayer = new UserController(socket.id, position, radius, massTotal, cells, type, c);
+
+    socket.on('gotit', function (player) {
+    
+    });
+
+    socket.on('pingcheck', function () {
+        socket.emit('pongcheck');
+    });
+
+    socket.on('respawn', function () {
+        if (util.findIndex(users, currentPlayer.id) > -1)
+            users.splice(util.findIndex(users, currentPlayer.id), 1);
+        socket.emit('welcome', currentPlayer);
+        console.log('[INFO] User ' + currentPlayer.name + ' respawned!');
+    });
+
+    socket.on('disconnect', function () {
+        if (util.findIndex(users, currentPlayer.id) > -1)
+            users.splice(util.findIndex(users, currentPlayer.id), 1);
+
+        socket.broadcast.emit('playerDisconnect', { name: currentPlayer.name });
+    });
+
+    socket.on('pass', function(data) {
+
+    });
+
+    socket.on('kick', function(data) {
+
+    });
+
+    // Heartbeat function, update everytime.
+    socket.on('heartbeat', function(target) {
+        currentPlayer.lastHeartbeat = new Date().getTime();
+        if (target.x !== currentPlayer.x || target.y !== currentPlayer.y) {
+            currentPlayer.target = target;
+        }
+    });
 });
 
-// function tick(currentPlayer) {
-//   let now = new DateTime.getTime();
-//   let maxHearbreakInterval = 1000; 
-  
-//   if (now - maxHearbreakInterval > currentPlayer.getLastHeartbreak()) {
-//     let socket = socket[currentPlayer.id];
 
-//     socket.emit('kick');
-//     socket.disconnect();
+setInterval(moveloop, 1000 / 60);
+setInterval(gameloop, 1000);
+setInterval(sendUpdates, 1000 / c.networkUpdateFactor);
 
-//     delete users[currentPlayer.id];
-//     delete sockets[currentPlayer.id];
-//   };
-// }
-
-// function gameloop() {
-//   console.log(users);
-//   // users.forEach(user => {
-//   //   tick(user);
-//   // });
-// }
-
-// setInterval(moveloop, 1000 / 60);
-// setInterval(gameloop, 1000);
-// setInterval(sendUpdates, 1000);
-
-// Don't touch, IP configurations.
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || config.host;
-var serverport = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || config.port;
-http.listen(serverport, ipaddress, function () {
+var ipaddress =  c.host;
+var serverport =  c.port;
+http.listen( serverport, ipaddress, function() {
     console.log('[DEBUG] Listening on ' + ipaddress + ':' + serverport);
 });
