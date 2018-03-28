1614f095079264f5869b2b445c3147e8a6a03041
Kosalexei <kos_ae@outlook.com>
2018-03-28 23:57:08 +0700

    Add SAT library for for performing collision detection. Bug fixes.

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 const io = require('socket.io')(http);
-
+var SAT = require('sat'); //Library for for performing collision detection.
 
 // Import game settings.
 var c = require('../../config.json');
@@ -13,82 +13,26 @@ var c = require('../../config.json');
 // Import utilities.
 var util = require('./lib/util');
 
-
-
 const UserController = require('./user_controller');
 const GameController = require('./game_controller');
+const {
+    connect
+} = require("./sockets");
+const UsersController = require("./users_controller");
 
+let usersController = new UsersController();
 var game = new GameController();
 
-var users = [];
+connect(io);
+
+global.sockets = {};
+
 var massFood = [];
 var food = [];
-var sockets = {};
 
 var initMassLog = util.log(c.defaultPlayerMass, c.slowBase);
 
 app.use(express.static(__dirname + '/../client'));
-
-io.on('connection', function(socket) {
-    console.log('A user connected!', socket.handshake.query.type);
-
-    var type = socket.handshake.query.type;
-    var radius = util.massToRadius(c.defaultPlayerMass);
-    var position = c.newPlayerInitialPosition == 'farthest' ? util.uniformPosition(users, radius) : util.randomPosition(radius);
-
-    var cells = [];
-    var massTotal = 0;
-    if(type === 'player') {
-        cells = [{
-            mass: c.defaultPlayerMass,
-            x: position.x,
-            y: position.y,
-            radius: radius
-        }];
-        massTotal = c.defaultPlayerMass;
-    }
-
-    let currentPlayer = new UserController(socket.id, position, radius, massTotal, cells, type, c);
-
-    socket.on('gotit', function (player) {
-    
-    });
-
-    socket.on('pingcheck', function () {
-        socket.emit('pongcheck');
-    });
-
-    socket.on('respawn', function () {
-        if (util.findIndex(users, currentPlayer.id) > -1)
-            users.splice(util.findIndex(users, currentPlayer.id), 1);
-        socket.emit('welcome', currentPlayer);
-        console.log('[INFO] User ' + currentPlayer.name + ' respawned!');
-    });
-
-    socket.on('disconnect', function () {
-        if (util.findIndex(users, currentPlayer.id) > -1)
-            users.splice(util.findIndex(users, currentPlayer.id), 1);
-
-        socket.broadcast.emit('playerDisconnect', { name: currentPlayer.name });
-    });
-
-    socket.on('pass', function(data) {
-
-    });
-
-    socket.on('kick', function(data) {
-
-    });
-
-    // Heartbeat function, update everytime.
-    socket.on('heartbeat', function(target) {
-        currentPlayer.lastHeartbeat = new Date().getTime();
-        if (target.x !== currentPlayer.x || target.y !== currentPlayer.y) {
-            currentPlayer.target = target;
-        }
-    });
-});
-
 
 function tickPlayer(currentPlayer) {
     if (currentPlayer.lastHeartbeat < new Date().getTime() - c.maxHeartbeatInterval) {
