589a960435d64a9d0260bb796fe275a437706eff
Kosalexei <kos_ae@outlook.com>
2018-03-28 23:54:08 +0700

    Move sockets to socket.js

--- src/server/sockets.js
+++ src/server/sockets.js
@@ -1,46 +1,120 @@
 'use strict';
 
 const socketio = require('socket.io');
+const PlayerController = require('./player_controller');
+var util = require('./lib/util');
+const UsersController = require("./users_controller");
 
-// const util = require('./lib/util');
-
-exports.connect = function (http, callback) {
-    const io = socketio(http);
+exports.connect = function (io) {
     io.on('connection', function (socket) {
-        callback(io, socket);
-    });
-}
+        console.log('A user connected!', socket.handshake.query.type);
 
-exports.handleAction = function (io, socket, currentPlayer) {
-    socket.on('heartbreak', function(target) {
-        console.log(currentPlayer);
-        currentPlayer.updateLastHeartbeat();
+        var type = socket.handshake.query.type;
+        var radius = util.massToRadius(conf.defaultPlayerMass);
+        var position = conf.newPlayerInitialPosition == 'farthest' ? util.uniformPosition(usersController.getUsers(), radius) : util.randomPosition(radius);
 
-        if (target.x !== currentPlayer.targetX || target.y !== currentPlayer.targetY) {
-            currentPlayer.targetX = target.x;
-            currentPlayer.targetY = target.y;
+        var cells = [];
+        var massTotal = 0;
+        if (type !== 'spectate') {
+            cells = [{
+                mass: conf.defaultPlayerMass,
+                x: position.x,
+                y: position.y,
+                radius: radius
+            }];
+            massTotal = conf.defaultPlayerMass;
         }
+
+        let currentPlayer = new PlayerController(socket.id, position, radius, massTotal, cells, type, conf);
+
+        socket.on('pingcheck', function () {
+            socket.emit('pongcheck');
+        });
+
+        socket.on('gotit', function (player) {
+            console.log('[INFO] Player ' + player.name + ' connecting!');
+
+            if (!util.validNick(player.name)) {
+                socket.emit('kick', 'Invalid username.');
+                socket.disconnect();
+            } else {
+                console.log('[INFO] Player ' + player.name + ' connected!');
+                global.sockets[player.id] = socket;
+
+                var radius = util.massToRadius(conf.defaultPlayerMass);
+                var position = conf.newPlayerInitialPosition == 'farthest' ? util.uniformPosition(usersController.getUsers(), radius) : util.randomPosition(radius);
+
+                player.x = position.x;
+                player.y = position.y;
+                player.target.x = 0;
+                player.target.y = 0;
+                if (type === 'spectate') {
+                    player.cells = [];
+                    player.massTotal = 0;
+                } else {
+                    player.cells = [{
+                        mass: conf.defaultPlayerMass,
+                        x: position.x,
+                        y: position.y,
+                        radius: radius
+                    }];
+                    player.massTotal = conf.defaultPlayerMass;
+                }
+                player.hue = Math.round(Math.random() * 360);
+                currentPlayer = player;
+                currentPlayer.lastHeartbeat = new Date().getTime();
+                // global.users.push(currentPlayer);
+                usersController.addUser(currentPlayer);
+
+                io.emit('playerJoin', {
+                    name: currentPlayer.name
+                });
+
+                socket.emit('gameSetup', {
+                    gameWidth: conf.gameWidth,
+                    gameHeight: conf.gameHeight
+                });
+                console.log('Total players: ' + usersController.getUsersLength());
+            }
+
+        });
+
+        socket.on('windowResized', function (data) {
+            currentPlayer.screenWidth = data.screenWidth;
+            currentPlayer.screenHeight = data.screenHeight;
+        });
+
+        socket.on('respawn', function () {
+            let users = usersController.getUsers(),
+                index = util.findIndex(users, currentPlayer.id);
+            if (index > -1) {
+                usersController.removeUser(index);
+            }
+            socket.emit('welcome', currentPlayer);
+            console.log('[INFO] User ' + currentPlayer.name + ' respawned!');
+        });
+
+        socket.on('disconnect', function () {
+            let users = usersController.getUsers(),
+                index = util.findIndex(users, currentPlayer.id);
+            if (index > -1) {
+                usersController.removeUser(index);
+            }
+            console.log('[INFO] User ' + currentPlayer.name + ' disconnected!');
+
+            socket.broadcast.emit('playerDisconnect', {
+                name: currentPlayer.name
+            });
+        });
+
+        // Heartbeat function, update everytime.
+        socket.on('heartbeat', function (target) {
+            //console.log(target)
+            currentPlayer.lastHeartbeat = new Date().getTime();
+            // console.log(target)
+            if (target.x !== currentPlayer.x || target.y !== currentPlayer.y) {
+                currentPlayer.target = target;
+            }
+        });
     });
-
-    socket.on('moveUp', function(direction) {
-      console.log('[INFO]: direction chagned - ', direction, socket.id);
-    })
-
-    socket.on('moveDown', function(direction) {
-      console.log('[INFO]: direction changed - ', direction, socket.id);
-    });
-
-    socket.on('moveRight', function(direction) {
-      console.log('[INFO]: direction changed - ', direction, socket.id);
-    });
-
-    socket.on('moveLeft', function(direction) {
-      console.log('[INFO]: direction changed - ', direction, socket.id);
-    });
-
-    socket.on('kick', function() {
-      //
-    });
-
-    socket.on('disconnect', function () {});
 }
