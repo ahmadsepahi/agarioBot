a487a384001c026d895f20d750b89f02ee9b3a08
ahmadsepahi <asepahi@umass.edu>
2020-06-08 06:37:41 -0400

    bot added, survey added need new db server

---
+++ Bot/clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pkill -f main.py
+pkill -f scary_bot.py
---
+++ Bot/runBot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in {1..5}
+do
+   /usr/local/opt/python@3.8/bin/python3 /Users/ahmad/agario/Bot/main.py &
+done
